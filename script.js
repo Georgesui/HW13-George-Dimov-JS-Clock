90b593a9c6d0d33ea7fb817f14987ca51208d06a
Georgesui <gdimov445@gmail.com>
2022-05-23 21:05:06 +0300

    final commit

--- script.js
+++ script.js
@@ -1,63 +1,27 @@
-// Обычный вариант
 function SetTime(element) {
 	this.element = element;
 	this.fullFormat = true;
-
-	this.render = function () {
-		if (this.fullFormat) {
-			this.element.innerHTML = new Date().toTimeString().split(' ')[0];
-		}
-		else {
-			this.element.innerHTML = new Date().toTimeString().split(' ')[0].substring(0, 5);
-		}
-	}
-
-	this.changeVersion = function () {
-		this.fullFormat = !this.fullFormat;
-		this.element.classList.toggle('header__clock-small');
-	}
-
 	this.element.addEventListener('click', () => {
 		this.changeVersion();
 	})
 }
 
+SetTime.prototype.render = function () {
+	if (this.fullFormat) {
+		return this.element.innerHTML = new Date().toTimeString().split(' ')[0];
+	} else {
+		return this.element.innerHTML = new Date().toTimeString().split(' ')[0].substring(0, 5);
+	}
+}
 
-// Вариант через prototype - тоже работает.
-
-// function SetTime(element) {
-// 	this.element = element;
-// 	this.fullFormat = true;
-// 	this.element.addEventListener('click', () => {
-// 		this.changeVersion();
-// 	})
-// }
-
-// SetTime.prototype.render = function () {
-// 	if (this.fullFormat) {
-// 		this.element.innerHTML = new Date().toTimeString().split(' ')[0];
-// 	}
-// 	else {
-// 		this.element.innerHTML = new Date().toTimeString().split(' ')[0].substring(0, 5);
-// 	}
-// }
-
-// SetTime.prototype.changeVersion = function () {
-// 	this.fullFormat = !this.fullFormat;
-// 	this.element.classList.toggle('header__clock-small');
-// }
-
-// Вызываем без задержки 1000 мс во избежании некрасивого перехода часов из одного формата в другой при клике.
+SetTime.prototype.changeVersion = function () {
+	this.fullFormat = !this.fullFormat;
+	this.element.classList.toggle('header__clock-small');
+}
 
 let firstClockOnPage = document.querySelector('.header__clock');
 let firstResultOfClock = new SetTime(firstClockOnPage);
 setInterval(() => firstResultOfClock.render(),);
-
-// Для решения пункта 4.5 был использован метод "changeVersion()" - его можно вызвать и он будет работать - 
-// firstClockOnPage.changeVersion();
-
-// 4.6 Чтобы убедиться, что функция-конструктор применима и универсальна.
-// Вторые вызвал с задержкой/
 
 let secondClockOnPage = document.querySelector('.header__clock-second');
 let secondResultOfClock = new SetTime(secondClockOnPage);
